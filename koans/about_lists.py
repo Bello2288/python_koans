01da58bbe2dc9df4302ea44c03e2040f4f3ed894
Gregg Bellofatto <greggbellofatto@Greggs-MacBook-Pro.local>
2022-10-06 09:00:40 -0400

    python koans updated, done with none, working on list

--- koans/about_lists.py
+++ koans/about_lists.py
@@ -10,8 +10,11 @@ from runner.koan import *
 class AboutLists(Koan):
     def test_creating_lists(self):
         empty_list = list()
-        self.assertEqual(list, type(empty_list))
-        self.assertEqual(__, len(empty_list))
+        self.assertEqual(list, type(empty_list)) # type of empty_list is a list
+        self.assertEqual(0, len(empty_list))  # current length of the list is zero
+
+
+# --------------------------------------------------------------------------------------
 
     def test_list_literals(self):
         nums = list()
@@ -21,48 +24,78 @@ class AboutLists(Koan):
         self.assertEqual([1], nums)
 
         nums[1:] = [2]
-        self.assertListEqual([1, __], nums)
+        self.assertListEqual([1, 2], nums)
 
-        nums.append(333)
-        self.assertListEqual([1, 2, __], nums)
+        nums.append(333)  # <----- .append will add the 333 to the end of the list
+        self.assertListEqual([1, 2, 333], nums)
+
+
+# --------------------------------------------------------------------------------------
+
 
     def test_accessing_list_elements(self):
         noms = ['peanut', 'butter', 'and', 'jelly']
+        # index    0         1        2       3     ##
+        # index   -4        -3      -2      -1      ##
+        self.assertEqual('peanut', noms[0]) # display index 0
+        self.assertEqual('jelly', noms[3]) # display index 3
+        self.assertEqual('jelly', noms[-1]) # display index -1
+        self.assertEqual('butter', noms[-3]) # display index -3
 
-        self.assertEqual(__, noms[0])
-        self.assertEqual(__, noms[3])
-        self.assertEqual(__, noms[-1])
-        self.assertEqual(__, noms[-3])
+
+# --------------------------------------------------------------------------------------
 
     def test_slicing_lists(self):
         noms = ['peanut', 'butter', 'and', 'jelly']
 
-        self.assertEqual(__, noms[0:1])
-        self.assertEqual(__, noms[0:2])
-        self.assertEqual(__, noms[2:2])
-        self.assertEqual(__, noms[2:20])
-        self.assertEqual(__, noms[4:0])
-        self.assertEqual(__, noms[4:100])
-        self.assertEqual(__, noms[5:0])
+        self.assertEqual(["peanut"], noms[0:1])             #\
+        self.assertEqual(["peanut", "butter"], noms[0:2])   # \
+        self.assertEqual([], noms[2:2])                     #  found info at https://www.geeksforgeeks.org/python-list-slicing/
+        self.assertEqual(["and", "jelly"], noms[2:20])      # /
+        self.assertEqual([], noms[4:0])                     #/
+        self.assertEqual([], noms[4:100])
+        self.assertEqual([], noms[5:0])
+
+
+# --------------------------------------------------------------------------------------
 
     def test_slicing_to_the_edge(self):
         noms = ['peanut', 'butter', 'and', 'jelly']
 
-        self.assertEqual(__, noms[2:])
-        self.assertEqual(__, noms[:2])
+        self.assertEqual(["and", "jelly"], noms[2:])
+         # ^^ this would read, start at index 2 and leave the rest of the list
+        self.assertEqual(["peanut", "butter"], noms[:2])
+        # ^^ this would read, start at the beginning and go up to but not include index 2
+
+
+# --------------------------------------------------------------------------------------
 
     def test_lists_and_ranges(self):
-        self.assertEqual(range, type(range(5)))
-        self.assertNotEqual([1, 2, 3, 4, 5], range(1,6))
-        self.assertEqual(__, list(range(5)))
-        self.assertEqual(__, list(range(5, 9)))
+        self.assertEqual(range, type(range(5))) # type of "range" is a range
+        self.assertNotEqual([1, 2, 3, 4, 5], range(1,6)) 
+        # this range will start at the number 1 and up to but not including 6
+        self.assertEqual([0, 1, 2, 3, 4], list(range(5))) 
+        # this range will give a range of 5 number starting at zero
+        self.assertEqual([5, 6, 7, 8], list(range(5, 9))) 
+        # this range will start at the number 5 and up to but not including 9
+
+# --------------------------------------------------------------------------------------
 
     def test_ranges_with_steps(self):
-        self.assertEqual(__, list(range(5, 3, -1)))
-        self.assertEqual(__, list(range(0, 8, 2)))
-        self.assertEqual(__, list(range(1, 8, 3)))
-        self.assertEqual(__, list(range(5, -7, -4)))
-        self.assertEqual(__, list(range(5, -8, -4)))
+        self.assertEqual([5, 4], list(range(5, 3, -1)))
+    # this range will start at the number 5, up to but not including 3, and stepping backwards by 1 number
+        self.assertEqual([0, 2, 4, 6], range(0, 8, 2))
+    # this range will start at the number 2, up to but not including 8, stepping 2 numbers
+        self.assertEqual([1, 4, 7], range(1, 8, 3))
+    # this range will start at the number 1, up to but not including 8, stepping 3 numbers
+        self.assertEqual([5, 1, -3], range(5, -7, -4))
+    # this range will start at the number 5, up to but not including -7, stepping backwards by 3 number
+        self.assertEqual([5, 1, -3, -7], range(5, -8, -4))
+    # this range will start at the number 5, up to but not including -8, stepping backwards by 4 number
+    
+
+
+# --------------------------------------------------------------------------------------
 
     def test_insertions(self):
         knight = ['you', 'shall', 'pass']
@@ -71,6 +104,9 @@ class AboutLists(Koan):
 
         knight.insert(0, 'Arthur')
         self.assertEqual(__, knight)
+
+
+# --------------------------------------------------------------------------------------
 
     def test_popping_lists(self):
         stack = [10, 20, 30, 40]
@@ -93,6 +129,9 @@ class AboutLists(Koan):
 
         # To learn more about this try typing "import this" from the python
         # console... ;)
+
+
+# --------------------------------------------------------------------------------------
 
     def test_making_queues(self):
         queue = [1, 2]
