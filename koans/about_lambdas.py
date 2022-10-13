b0c1154639bc361e47c4b4da8af12180e0ed0e15
Gregg Bellofatto <greggbellofatto@Greggs-MacBook-Pro.local>
2022-10-13 16:06:19 -0400

    finished koans about_generators and about_lambdas

--- koans/about_lambdas.py
+++ koans/about_lambdas.py
@@ -8,9 +8,18 @@
 from runner.koan import *
 
 class AboutLambdas(Koan):
+
+# https://docs.quantifiedcode.com/python-anti-patterns/correctness/assigning_a_lambda_to_a_variable.html
+# https://realpython.com/python-lambda/
+# https://www.analyticsvidhya.com/blog/2020/03/what-are-lambda-functions-in-python/
+
     def test_lambdas_can_be_assigned_to_variables_and_called_explicitly(self):
         add_one = lambda n: n + 1
-        self.assertEqual(__, add_one(10))
+        # self.assertEqual(__, add_one(10))
+        self.assertEqual(11, add_one(10))
+
+
+
 
     # ------------------------------------------------------------------
 
@@ -21,8 +30,11 @@ class AboutLambdas(Koan):
         sausages = self.make_order('sausage')
         eggs = self.make_order('egg')
 
-        self.assertEqual(__, sausages(3))
-        self.assertEqual(__, eggs(2))
+        # self.assertEqual(__, sausages(3))
+        # self.assertEqual(__, eggs(2))
+        self.assertEqual('3 sausages', sausages(3))
+        self.assertEqual('2 eggs', eggs(2))
 
     def test_accessing_lambda_without_assignment(self):
-        self.assertEqual(__, self.make_order('spam')(39823))
+        # self.assertEqual(__, self.make_order('spam')(39823))
+        self.assertEqual('39823 spams', self.make_order('spam')(39823))
